88462bf08d9aed4222f305a23bb456227cd1a5ac
Dongming Zhang <dzhang123@gmail.com>
2019-01-09 13:29:17 -0600

    reset endpoint to local

--- dev-sensor-groups_create-table.js
+++ dev-sensor-groups_create-table.js
@@ -5,7 +5,7 @@ var AWS = require("aws-sdk");
 
 AWS.config.update({
     region: "us-east-1",
-    //endpoint: "http://localhost:3306"
+    endpoint: "http://localhost:3306"
 });
 
 var dynamodb = new AWS.DynamoDB();
